1df910b3b4af590d40facfaace1663a7b3443c17
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-02 17:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function (array) { return array[0] + " " + array[1] }
+
+const returnFirstTwoDrivers = function (array) { return [array[0], array[1]] }
