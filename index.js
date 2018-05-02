f5c888939ccc33b749fa9472775783b068be7f04
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-02 17:22:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 
 const returnFirstTwoDrivers = function (array) { return [array[0], array[1]] }
 
-const returnLastTwoDrivers = function (array) { return [array[-2], array[-1]] }
+const returnLastTwoDrivers = function (array) { return array.slice(-2) }
